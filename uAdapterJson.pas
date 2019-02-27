3e5c6371ca7675ddb2ec5c1cb7a44188bb732bd6
jesus.mayuri.cuellar@gmail.com <JesusMayuri7>
2019-02-26 20:29:16 -0500

    RestAdapter Json Arbitario

---
+++ uAdapterJson.pas
@@ -0,0 +1,64 @@
+unit uAdapterJson;
+
+interface
+
+uses System.JSON, REST.JSON, REST.Client,System.Classes;
+
+type
+  TAdapterJSONValue =  class(TInterfacedObject, IRESTResponseJSON)
+  private
+    FJSONValue: TJSONValue;
+  protected
+    { IRESTResponseJSON }
+    procedure AddJSONChangedEvent(const ANotify: TNotifyEvent);
+    procedure RemoveJSONChangedEvent(const ANotify: TNotifyEvent);
+    procedure GetJSONResponse(out AJSONValue: TJSONValue; out AHasOwner: Boolean);
+    function HasJSONResponse: Boolean;
+    function HasResponseContent: Boolean;
+  public
+    constructor Create(const AJSONValue: TJSONValue);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+procedure TAdapterJSONValue.AddJSONChangedEvent(const ANotify: TNotifyEvent);
+begin
+  // Not implemented because we pass JSON in constructor and do not change it
+end;
+
+constructor TAdapterJSONValue.Create(const AJSONValue: TJSONValue);
+begin
+  FJSONValue := AJSONValue;
+end;
+
+destructor TAdapterJSONValue.Destroy;
+begin
+  // We own the JSONValue, so free it.
+  FJSONValue.Free;
+  inherited;
+end;
+
+procedure TAdapterJSONValue.GetJSONResponse(out AJSONValue: TJSONValue;
+  out AHasOwner: Boolean);
+begin
+  AJSONValue := FJSONValue;
+  AHasOwner := True; // We own this object
+end;
+
+function TAdapterJSONValue.HasJSONResponse: Boolean;
+begin
+  Result := FJSONValue <> nil;
+end;
+
+function TAdapterJSONValue.HasResponseContent: Boolean;
+begin
+  Result := FJSONValue <> nil;
+end;
+
+procedure TAdapterJSONValue.RemoveJSONChangedEvent(const ANotify: TNotifyEvent);
+begin
+  // Not implemented because we pass JSON in constructor and do not change it
+end;
+
+end.
